6c65991715e6bb081c35b089989288dcf41d2dfc
Nik Cubrilovic <nikcub@gmail.com>
2011-12-16 03:04:05 +1100

    new css build script

--- static/css/build.sh
+++ static/css/build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-SOURCES=(site admin)
+SOURCES="site admin"
 LESS_BIN=`which lessc`
 WATCHR_BIN=`which watchr`
 
-if [ -e $LESS_BIN ]; then
+if [ -z $LESS_BIN ]; then
 	echo "Could not find less compressor"
 	exit -1
-done
+fi
 
-for SRC in SOURCES
-do
+for SRC in $SOURCES; do
 	if [ -e "less/$SRC.less" ]; then
 		echo "Building: less/$SRC.less"
+		$LESS_BIN "less/$SRC.less" > $SRC.css 
+		$LESS_BIN "less/$SRC.less" > $SRC.min.css --compress
 	else
 		echo "Fatal: $SRC not a file"
 	fi
 done
+
+echo "Done"
